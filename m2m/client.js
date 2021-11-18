abdc0c19efce40a9d5177f3f10cd8f04624d03c6
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2021-11-18 09:28:15 -0500

    Add files via upload

--- m2m/client.js
+++ m2m/client.js
@@ -1,1 +1,9 @@
-test
+const { Client } = require('m2m');
+
+const client = new Client({name:'Client Demo', location:'Anywhere', description:'m2m web application integration'});
+
+client.connect('https://dev.node-m2m.com');
+
+module.exports = client;
+
+  
