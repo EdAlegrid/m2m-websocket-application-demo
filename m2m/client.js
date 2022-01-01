12d4d5b688188bb40773bd09d322c596623b62eb
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-01-01 07:27:00 -0500

    Update client.js

--- m2m/client.js
+++ m2m/client.js
@@ -2,7 +2,7 @@ const { Client } = require('m2m');
 
 const client = new Client({name:'Client Demo', location:'Anywhere', description:'m2m websocket application integration'});
 
-client.connect('https://dev.node-m2m.com');
+client.connect('https://www.node-m2m.com');
 
 module.exports = client;
 
